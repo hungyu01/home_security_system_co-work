a0ebf2045924fde8e7bfc0a9f472d5401f0e5caf
Lin_HY <linhungyu0328@gmail.com>
2024-05-30 15:56:54 +0800

    opencv視訊畫面(待優化)

--- app.js
+++ app.js
@@ -4,28 +4,31 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
-const {DBHOST, DBPORT, DBNAME} = require('./config/config');
+const { DBHOST, DBPORT, DBNAME } = require('./config/config');
 const indexRouter = require('./routes/web/index');
 const authRouter = require('./routes/web/auth');
-const authApiRouter = require('./routes/api/auth')
-//導入 account api 路由文件
+const authApiRouter = require('./routes/api/auth');
+//导入 account api 路由文件
 const accountRouter = require('./routes/api/account');
+//导入 Socket.IO
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
 
-//設定 session 的中間件
+//设置 session 的中间件
 app.use(session({
-  name: 'sid', //設定 cookie 的 name，預設是: connect.sid
-  secret: 'aiml05_02', //參與加密的字符串(又稱為簽名)
-  saveUninitialized: false, //是否在每次請求時都設定一個 cookie 儲存 session id
-  resave: true, //在每次請求後重新保存 session
-  store: MongoStore.create({
-      mongoUrl: `mongodb://${DBHOST}:${DBPORT}/${DBNAME}` //資料庫的連接
-  }),
-  cookie:{
-      httpOnly:true, //開啟後前端無法透過 JS 操作
-      maxAge: 1000*60*60*24*7 //控制 session 過期的時間
-  },
+    name: 'sid', //设置 cookie 的 name，默认是: connect.sid
+    secret: 'aiml05_02', //参与加密的字符串(又称为签名)
+    saveUninitialized: false, //是否在每次请求时都设置一个 cookie 储存 session id
+    resave: true, //在每次请求后重新保存 session
+    store: MongoStore.create({
+        mongoUrl: `mongodb://${DBHOST}:${DBPORT}/${DBNAME}` //数据库的连接
+    }),
+    cookie: {
+        httpOnly: true, //开启后前端无法通过 JS 操作
+        maxAge: 1000 * 60 * 60 * 24 * 7 //控制 session 过期的时间
+    },
 }));
 
 // view engine setup
@@ -38,28 +41,43 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter); //首頁及功能頁面
-app.use('/', authRouter); //註冊功能頁面
-app.use('/api', accountRouter); 
-app.use('/api', authApiRouter); 
+app.use('/', indexRouter); //首页及功能页面
+app.use('/', authRouter); //注册功能页面
+app.use('/api', accountRouter);
+app.use('/api', authApiRouter);
 
+// 创建 HTTP 服务器
+const server = http.createServer(app);
+
+// 创建 Socket.IO 服务器
+const io = new Server(server);
+
+// WebSocket 处理
+io.on('connection', (socket) => {
+    console.log('WebSocket client connected');
+    socket.on('stream', (data) => {
+        io.emit('stream', data);
+    });
+    socket.on('disconnect', () => {
+        console.log('WebSocket client disconnected');
+    });
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  //回應404
-  res.render('404');
+app.use(function (req, res, next) {
+    //响应404
+    res.render('404');
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
-
-module.exports = app;
+module.exports = { app, server }; // 导出 app 和 server
